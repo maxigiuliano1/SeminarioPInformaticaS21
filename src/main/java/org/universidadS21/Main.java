29d717ae4ae5d82b8951129ba7d411510f4eefd1
Giuliano <maximiliano.giuliano@accenture.com>
2025-06-08 16:51:28 -0300

    Correcciones en el main

--- src/main/java/org/universidadS21/Main.java
+++ src/main/java/org/universidadS21/Main.java
@@ -1,20 +1,15 @@
 package org.universidadS21;
 
-import org.universidadS21.controller.ImportarController;
-import org.universidadS21.controller.ProductoController;
 import org.universidadS21.model.Producto;
 import org.universidadS21.utils.ImportadorExcel;
 import org.universidadS21.view.MenuConsola;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         //String ruta = "C:\\Users\\maximiliano.giuliano\\Desktop\\lista_precios.xlsx";
-        //List<Producto> productos = ImportadorExcel.leerListaPrecios(ruta); //no hace falta ya que puedo llamarlo directamente en insertar y actualizar productos
-        //ImportarController importarController = new ImportarController();
-        //importarController.insertarYActualizarProducto(ruta);
+        //List<Producto> productos = ImportadorExcel.leerListaPrecios(ruta);
 
         MenuConsola menu = new MenuConsola();
         menu.iniciar();
