e7298e5d09787d57e87cd726378532b77c7ca5ee
Giuliano <maximiliano.giuliano@accenture.com>
2025-06-29 17:11:22 -0300

    Se corrigio el main y se eliminaron las clases que antes hacian que el proyecto funcione por consola

--- src/main/java/org/universidadS21/Main.java
+++ src/main/java/org/universidadS21/Main.java
@@ -1,14 +1,9 @@
 package org.universidadS21;
 
-import org.universidadS21.exceptions.BaseDatosException;
 import org.universidadS21.view.swing.VentanaPrincipal;
 
-import java.sql.SQLException;
-
 public class Main {
-    public static void main(String[] args) throws SQLException, BaseDatosException {
-        //MenuConsola menu = new MenuConsola();
-        //menu.iniciar();
+    public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(()->{
             VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
             ventanaPrincipal.setVisible(true);
